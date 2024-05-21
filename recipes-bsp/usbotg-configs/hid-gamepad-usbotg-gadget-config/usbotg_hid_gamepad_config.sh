b627330ea01590d4d7b0808b41e5b90af4ca13ae
Tom Moore <tomwmoore@hotmail.com>
2024-05-21 20:04:30 +0100

    usbotg-configs: Added HID gamepad usbotg gadget config.

---
+++ recipes-bsp/usbotg-configs/hid-gamepad-usbotg-gadget-config/usbotg_hid_gamepad_config.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+configfs="/sys/kernel/config/usb_gadget"
+g=g1
+c=c.1
+d="${configfs}/${g}"
+func_hid_gamepad=hid.0
+
+VENDOR_ID="0x1d6b" # Linux Foundation 
+PRODUCT_ID="0x0104" # Multifunction Composite Gadget
+
+do_start() {
+    if [ ! -d ${configfs} ]; then
+        modprobe libcomposite
+        if [ ! -d ${configfs} ]; then
+        exit 1
+        fi
+    fi
+
+    if [ -d ${d} ]; then
+        exit 0
+    fi
+
+    udc=$(ls -1 -I dummy_udc* /sys/class/udc/)
+    if [ -z $udc ]; then
+        echo "No UDC driver registered"
+        exit 1
+    fi
+
+    # Create gadget directory
+    mkdir "${d}"
+
+    # Assign USB ids
+    echo ${VENDOR_ID} > "${d}/idVendor"
+    echo ${PRODUCT_ID} > "${d}/idProduct"
+    echo 0x0200 > "${d}/bcdUSB"
+    # Windows extension to use IAD (Interface Association Descriptor)
+    # https://learn.microsoft.com/en-us/windows-hardware/drivers/usbcon/usb-interface-association-descriptor
+    echo "0xEF" > "${d}/bDeviceClass"
+    echo "0x02" > "${d}/bDeviceSubClass"
+    echo "0x01" > "${d}/bDeviceProtocol"
+    echo "0x0100" > "${d}/bcdDevice"
+
+    # Assign USB strings (optional)
+    mkdir -p "${d}/strings/0x409"
+    tr -d '\0' < /proc/device-tree/serial-number > "${d}/strings/0x409/serialnumber"
+    echo "Tom's" > "${d}/strings/0x409/manufacturer"
+    echo "Gamepad" > "${d}/strings/0x409/product"
+    echo "0123456789abcdef" > "${d}/strings/0x409/serialnumber"
+
+    # Create gadget configuration
+    mkdir -p "${d}/configs/${c}"
+    #Set English Strings
+    mkdir -p "${d}/configs/${c}/strings/0x409"
+
+    # Create gadget hid mouse function
+    mkdir -p "${d}/functions/${func_hid_gamepad}"
+    echo 1 > "${d}/functions/${func_hid_gamepad}/subclass"
+    echo 2 > "${d}/functions/${func_hid_gamepad}/protocol"
+    echo 8 > "${d}/functions/${func_hid_gamepad}/report_length"
+    echo -ne \\x05\\x01\\x09\\x05\\xA1\\x01\\xA1\\x00\\x85\\x01\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x01\\x26\\xff\\x07\\x75\\x10\\x95\\x02\\x81\\x02\\x05\\x09\\x19\\x01\\x29\\x0F\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x0F\\x81\\x02\\xC0\\xC0 > "${d}/functions/${func_hid_gamepad}/report_desc"
+
+    # 0x05, 0x01,                    // Usage Page (Generic Desktop)
+    # 0x09, 0x05,                    // Usage (Game Pad)
+    # 0xa1, 0x01,                    // Collection (Application)
+    # 0xa1, 0x00,                    //   Collection (Physical)
+    # 0x85, 0x01,                    //     Report ID (1)
+    # 0x05, 0x01,                    //     Usage Page (Generic Desktop)
+    # 0x09, 0x30,                    //     Usage (X)
+    # 0x09, 0x31,                    //     Usage (Y)
+    # 0x15, x01,                     //     Logical Minimum (1)
+    # 0x26, 0xff, 0x07,              //     Logical Maximum (2047)
+    # 0x75, 0x10,                    //     Report Size (16)
+    # 0x95, 0x02,                    //     Report Count (2)
+    # 0x81, 0x02,                    //     Input (Data,Var,Abs)
+    # 0x05, 0x09,                    //     Usage Page (Button)
+    # 0x19, 0x01,                    //     Usage Minimum (1)
+    # 0x29, 0x0F,                    //     Usage Maximum (15)
+    # 0x15, 0x00,                    //     Logical Minimum (0)
+    # 0x25, 0x01,                    //     Logical Maximum (1)
+    # 0x75, 0x01,                    //     Report Size (1)
+    # 0x95, 0x0F,                    //     Report Count (15)
+    # 0x81, 0x02,                    //     Input (Data,Var,Abs)
+    # 0xc0,                          //   End Collection
+    # 0xc0                           // End Collection
+
+    # Assign function to configuration
+    ln -s "${d}/functions/${func_hid_gamepad}" "${d}/configs/${c}"
+
+    # Bind to UDC
+    echo "${udc}" > "${d}/UDC"
+}
+
+do_stop() {
+    # Unbind the UDC
+    echo "" > "${d}/UDC"
+
+    # # Teardown gadget directory
+    [ -d "${d}/configs/${c}/${func_hid_gamepad}" ] && rm -f "${d}/configs/${c}/${func_hid_gamepad}"
+
+    [ -d "${d}/strings/0x409/" ] && rmdir "${d}/strings/0x409/"
+    [ -d "${d}/configs/${c}/strings/0x409" ] && rmdir "${d}/configs/${c}/strings/0x409"
+    [ -d "${d}/configs/${c}" ] && rmdir "${d}/configs/${c}"
+    [ -d "${d}/functions/${func_hid_gamepad}" ] && rmdir "${d}/functions/${func_hid_gamepad}"
+    [ -d "${d}" ] && rmdir "${d}"
+}
+
+case $1 in
+    start)
+        echo "Start hid gamepad gadget"
+        do_start
+        ;;
+    stop)
+        echo "Stop hid gamepad gadget"
+        do_stop
+        ;;
+    restart)
+        echo "Stop hid gamepad gadget"
+        do_stop
+        sleep 1
+        echo "Start hid gamepad gadget"
+        do_start
+        ;;
+    *)
+        echo "Usage: $0 (stop | start | restart)"
+        ;;
+esac
